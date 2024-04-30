67ec5f3844df1795587594306bb74918f0afa465
PROAÑOS ACOSTA NELSON ALEJANDRO <nproanos71@unisalle.edu.co>
2024-04-30 12:01:19 -0500

    first push

--- model.py
+++ model.py
@@ -1,33 +1,28 @@
-import streamlit as st
-import numpy as np
 import pandas as pd
-from sklearn.tree import DecisionTreeRegressor
-np.random.seed(1234)
-st.title('RShelp')
-datos=np.random.normal(0,1, size=(100,4))
-data=pd.DataFrame(datos,
-                  columns=list('ABCD'))
-st.dataframe(data)
-e=np.random.normal(0,1, size =100)
-y=data['A']*2 + data['B']*3+ data['C']*4+ data['D']*0.3 +10 +e
-model = DecisionTreeRegressor(max_depth=4)
-model.fit(data,y)
-st.subheader('A')
-val_a=st.slider('Seleccione el valor de A',
-          data['A'].min(),
-          data['A'].max())
-st.subheader('B')
-val_b=st.slider('Seleccione el valor de B',
-          data['B'].min(),
-          data['B'].max())
-st.subheader('C')
-val_c=st.slider('Seleccione el valor de C',
-          data['C'].min(),
-          data['C'].max())
-st.subheader('D')
-val_d=st.slider('Seleccione el valor de D',
-          data['D'].min(),
-          data['D'].max())
-valores=np.array([[val_a,val_b,val_c,val_d]])
-pre=model.predict(valores)
-st.write(pre)
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import mean_squared_error
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.model_selection import GridSearchCV
+import pickle
+data = pd.read_csv("hprice.csv")
+print(data)
+X = data.drop("price", axis=1)
+y = data["price"]
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+param_grid = {
+    "n_estimators": [100, 200, 500],
+    "max_depth": [4, 8, 16],
+    "random_state": [42]
+    }
+rf_regressor = RandomForestRegressor()
+grid_search = GridSearchCV(estimator=rf_regressor, param_grid=param_grid, scoring="neg_mean_squared_error")
+
+grid_search.fit(X_train, y_train)
+
+best_model = grid_search.best_estimator_
+
+y_pred = best_model.predict(X_test)
+rmse = mean_squared_error(y_test, y_pred)**0.5
+print("RMSE de RandomForestRegressor:", rmse)
+with open("model.pickle", "wb") as f:
+    pickle.dump(model, f)
